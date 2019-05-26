40180cc0a6330027175960a96ca3a8835fb82e7f
martin <martonlineca@gmail.com>
2019-05-25 22:48:08 -0400

    05252019

---
+++ 05252019-node-javascript/module1.js
@@ -0,0 +1,9 @@
+//exports functions or variables one by one
+function myFunction() {
+  console.log('Function was called');
+}
+
+var myString = 'String';
+
+module.exports.myFunction = myFunction;
+module.exports.myString = myString;
---
+++ 05252019-node-javascript/module2.js
@@ -0,0 +1,7 @@
+//exports all the functions or vaiables at the one time.
+module.exports = {
+  myFunction: function () {
+    console.log('module2.js ::: myFunction');
+  },
+  myVariable: 'module2.js ::: myVariable'
+}
